15397904aa42d7cb8916338cd625db82f97ed302
gitqiyun <1114260199@qq.com>
2019-12-03 17:52:59 +0800

    源码

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,28 @@
+import org.apache.log4j.Logger;
+
+/**
+ * create: qiyun
+ * date: 2019/12/3
+ * desc:源码运行示例
+ */
+public class HelloWorld {
+
+    private static Logger logger =Logger.getLogger(HelloWorld.class);
+
+    public static void main(String[] args) {
+        logger.info("你好");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
